84c2981064dec56e6a3d5357ab4e8f65859ab17d
AB <alejandro.boccardi@gmail.com>
2016-12-01 15:58:45 -0300

    Ajustes para definir el nombre de la base de datos de eventos a consultar segun la fecha a consultar

--- public/js/historian/historian.js
+++ public/js/historian/historian.js
@@ -27,8 +27,15 @@ var Historian  = function () {
 			var top = $( 'input[name=historicQuery]:checked' ).val();
 			console.log("called "+top);
 			var queryParam = {};
+			var dbhost = "5d594679-db64-4ed8-a0d8-1cd09196d918-bluemix.cloudant.com"
+			var dbid = "/iotp_znuylb_default_"
+			var apicmd = "/_design/iotp/_view/by-deviceId-and-date"
+			var user = "5d594679-db64-4ed8-a0d8-1cd09196d918-bluemix";
+			var pass = "4bac2881ad82100778fc1b0d7e34f6066a7c718a7d12baf8acf02504d5cf1c87";
 			
 			if(top == "topEvents") {
+				var d = new Date();
+				dbid = dbid + d.getFullYear() + "-" + (d.getMonth()+1);
 				var limite = $(historicTopRange).spinner( "value" );
 				var skey = '["'+tokens[3]+'",{}]';
 				var ekey = '["'+tokens[3]+'"]';
@@ -38,19 +45,18 @@ var Historian  = function () {
 				//Datetimes only in GMT
 				var fechaini = new Date(Date.parse($(historicStarts).val()));
 				var fechafin = new Date(Date.parse($(historicEnds).val()));
-				var limite = 1000;
+				dbid = dbid + fechafin.getFullYear() + "-" + (fechafin.getMonth()+1);
+				var limite = 100000;
 				var skey = '["'+tokens[3]+'","'+fechafin.toISOString()+'"]';
 				var ekey = '["'+tokens[3]+'","'+fechaini.toISOString()+'"]';
 				var desc = true;
 			}
-			var baseurl = "5d594679-db64-4ed8-a0d8-1cd09196d918-bluemix.cloudant.com/iotp_znuylb_default_2016-11/_design/iotp/_view/by-deviceId-and-date"
-			var user = "5d594679-db64-4ed8-a0d8-1cd09196d918-bluemix";
-			var pass = "4bac2881ad82100778fc1b0d7e34f6066a7c718a7d12baf8acf02504d5cf1c87";
+//			var baseurl = "5d594679-db64-4ed8-a0d8-1cd09196d918-bluemix.cloudant.com/iotp_znuylb_default_2016-11/_design/iotp/_view/by-deviceId-and-date"
 			
 			$.ajax
 			({
 				type: "GET",
-				url: "https://" + baseurl,
+				url: "https://" + dbhost + dbid + apicmd,
 				data: {
 					startkey: skey,
 					endkey: ekey,
